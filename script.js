eef7c060e7deea34e4441239cca72ea4e9006de6
ctkmpy <ahmadsaepudin032@gmail.com>
2023-05-10 11:00:36 +0700

    fixing the result

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var theMind;
-var inputMind = document.querySelector("input[name='clientMind']");
-
+const inputMind = document.querySelector("input[name='clientMind']");
 const askContainer = document.querySelector('.ask-container');
 const processContainer = document.querySelector('.process-container');
 const resultContainer = document.querySelector('.result-container');
@@ -8,26 +6,31 @@ const resultContainer = document.querySelector('.result-container');
 const yesGuess = document.getElementById('yesGuess');
 const noNvm = document.getElementById('noNvm');
 
-const result = document.querySelector('.result-container p');  
+const result = document.querySelector('.result-container input');  
 
+const submitTheMind = document.getElementById('submitTheMind');
 // Ask The Mind
-function claimTheMind() {
-    theMind = inputMind.value;
-    console.log(theMind);
-    askContainer.style.display = "none";
-    processContainer.style.display = "flex";
-};
+
+submitTheMind.addEventListener('click', () => {
+    result.value = inputMind.value;
+    if (inputMind.value == "") {
+        result.value = "i'm sorry but, your mind is blank. Are you OK?";
+    }
+    console.log(result.value);
+    askContainer.style.transform = "scale(0)";
+    processContainer.style.transform = "scale(1)";
+});
 
 // Procees
     yesGuess.addEventListener ('click', () => {
-        processContainer.style.display = "none";
+        processContainer.style.transform = "scale(0)";
         resultContainer.style.transform = "scale(1)";
-    } );
-    noNvm.addEventListener ('click', () => {
-        processContainer.style.display = "none";
-        askContainer.style.display = "flex";
     } );
 
 // Result
-    result.innerHTML = theMind;
-    alert(theMind); 
+    function backTo() {
+        processContainer.style.transform = "scale(0)";
+        resultContainer.style.transform = "scale(0)";
+        askContainer.style.transform = "scale(1)";
+        inputMind.value = "";
+    }
